57f8acea55dc0ce22b8c3e824b4e399fd1a17f61
mrevanishere <mrevanishere@gmail.com>
2021-03-23 23:10:03 -0700

    backend works (dev only)

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+// const baseUrl = 'http://localhost:3001/notes'
+// part 3:
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
